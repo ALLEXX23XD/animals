51da8057275dbcee6d28a4e983a0b9fc5f39b64e
ALLEXX23XD <alexandefhcp@gmail.com>
2025-04-01 18:35:37 -0700

    agrege mas elementos a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
 import './App.css'
 
+import animalsShow from "./animalsShow"
+
+
+function getRandomAnimal(){
+  const animals= ['cat','bird', 'dog', 'cow', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random()* animals.length)]
+}
 function App() {
-  const [count, setCount] = useState(0)
+
+  const[animals, setAnimals]= useState([])
+  
+  const handleClick = ()=>{
+    setAnimals([...animals,getRandomAnimal()])
+  }
+
+  const renderAnimals= animals.map((animal, index) =>{
+    return <animalsShow type={animal} key={index}/>
+  })
+
 
   return (
-    <>
-     <h1>Animals App</h1>
-    </>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">
+        {renderAnimals}
+      </div>
+    </div>
   )
 }
 
